6e39fc89e9827d4b9eb70cf37d40f75cc134931c
yashu-20 <69629481+yashu-20@users.noreply.github.com>
2021-05-17 11:57:13 +0530

    Loops

---
+++ Exp 3.2.py
@@ -0,0 +1,19 @@
+'''
+3.2)
+Implement a python script using for loop that loops over a sequence.
+'''
+sequence = [1,2,3,4,5,6,7,8,9]
+for i in sequence :
+  print(i)
+'''
+Output
+1
+2
+3
+4
+5
+6
+7
+8
+9
+'''
